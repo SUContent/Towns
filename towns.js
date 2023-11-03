b319d561c0057a27e670a481c3f850edf733aa89
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-11-03 10:39:57 +0200

    add new town functionality

--- towns.js
+++ towns.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
 
+$(document).ready(function() {
+	$('#btnAdd').click(addTown)
+});
+
 function deleteTown() {
 	let townName = $('#townName').val();
 	$('#townName').val('');
@@ -16,4 +20,19 @@ function deleteTown() {
 		$('#result').text(townName + " deleted.");
 	else
 		$('#result').text(townName + " not found.");
+}
+
+function addTown() {
+	let townName = $('#town-name-add').val();
+	$('#town-name-add').val('');
+	let added = false;
+	
+	let newOption = $("<option>", {
+		value: townName,
+		text: townName
+	});
+
+	$('#towns').append(newOption);
+
+	$('#result').text(`${townName} added.`);
 }
