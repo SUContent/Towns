1f2527d2e0e5db6e4b308741b9f552525100668b
jivak1 <jivak1@abv.bg>
2023-11-08 01:44:11 +0200

    Implemented functionality to shuffle the existing towns

--- towns.js
+++ towns.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 	$('#btnAdd').click(addTown);
+	$('#btnShuffle').click(shuffleTowns);
 });
 
 function deleteTown() {
@@ -23,4 +24,21 @@ function addTown() {
 	let townName = $('#townNameForAdd').val(); $('#townNameForAdd').val('');
 	$('#towns').append($('<option>').text(townName)); 
 	$('#result').text(townName + " added."); 
+}
+
+function shuffleTowns() { 
+	let towns = $('#towns option').toArray(); 
+	$('#towns').empty();
+	shuffleArray(towns); 
+	$('#towns').append(towns); 
+	$('#result').text("Towns shuffled."); 
+
+	function shuffleArray(array) { 
+		for (var i = array.length - 1; i > 0; i--){
+				var j = Math.floor(Math.random() * (i + 1));
+			 	var oldElement = array[i]; 
+				array[i] = array[j]; 
+				array[j] = oldElement; 
+			} 
+	} 
 }
