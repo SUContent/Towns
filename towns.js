651c06e800a477822e2ff71db04bcb2521bca3a6
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-03-03 09:17:29 +0200

    create add button and add functionality

--- towns.js
+++ towns.js
@@ -1,19 +1,23 @@
-$(document).ready(function() {
-	$('#btnDelete').click(deleteTown)
+$(document).ready(function () {
+  $("#btnDelete").click(deleteTown);
+  $('#btnAdd').click(addTown);
 });
-
+function addTown() {
+  let townName = $("#townNameForAdd").val();
+  $("#townNameForAdd").val("");
+  $("#towns").append($("<option>").text(townName));
+  $("#result").text(townName + " added.");
+}
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+  let townName = $("#townName").val();
+  $("#townName").val("");
+  let removed = false;
+  for (let option of $("#towns option")) {
+    if (option.textContent == townName) {
+      removed = true;
+      option.remove();
+    }
+  }
+  if (removed) $("#result").text(townName + " deleted.");
+  else $("#result").text(townName + " not found.");
 }
