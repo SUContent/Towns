2db0a95e3bdc2237b8e6e123cbacf5beabafe47b
Murtada Ahmed <murtadhabg@gmail.com>
2024-03-03 17:58:08 +0200

    Implemented functionality to shuffle the existing towns

--- towns.js
+++ towns.js
@@ -17,3 +17,28 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+
+
+// Shuffler functionality
+
+$(document).ready(function() {
+	$('#btnShuffle').click(shuffleTowns);
+});
+
+function shuffleTowns() {
+	let towns = $('#towns option').toArray();
+	$('#towns').empty();
+	shuffleArray(towns);
+	$('#towns').append(towns);
+	$('#result').text("Towns shuffled.");
+
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var oldElement = array[i];
+			array[i] = array[j];
+			array[j] = oldElement;
+		}
+	}
+}
+
