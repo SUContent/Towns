533278e6c46f5c97b9e2291c5f807a98e777b2eb
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-03-03 10:04:51 +0200

    Add message funtionality

--- towns.js
+++ towns.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("#btnDelete").click(deleteTown);
-  $('#btnAdd').click(addTown);
+  $("#btnAdd").click(addTown);
 });
 function addTown() {
   let townName = $("#townNameForAdd").val();
@@ -20,4 +20,23 @@ function deleteTown() {
   }
   if (removed) $("#result").text(townName + " deleted.");
   else $("#result").text(townName + " not found.");
+}
+// function deleteTown() {
+//   let townName = $("#townName").val();
+//   $("#townName").val("");
+//   let removed = false;
+//   for (let option of $("#towns option")) {
+//     if (option.textContent == townName) {
+//       removed = true;
+//       option.remove();
+//     }
+//   }
+//   if (removed) showMessage(townName + " deleted.");
+//   else showMessage(townName + " not found.");
+// }
+function showMessage(msg) {
+  $("#result").text(msg).css("display", "block");
+  setTimeout(function () {
+    $("#result").hide("blind", {}, 500);
+  }, 3000);
 }
