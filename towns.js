d131e91318970da58ac8e448f39e942c5ef785ac
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-03-03 10:15:08 +0200

    add shuffle functionality

--- towns.js
+++ towns.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   $("#btnDelete").click(deleteTown);
   $("#btnAdd").click(addTown);
+  $(document).ready(function() { $('#btnShuffle').click(shuffleTowns); })
 });
 function addTown() {
   let townName = $("#townNameForAdd").val();
@@ -20,6 +21,22 @@ function deleteTown() {
   }
   if (removed) $("#result").text(townName + " deleted.");
   else $("#result").text(townName + " not found.");
+}
+function shuffleTowns() {
+  let towns = $("#towns option").toArray();
+  $("#towns").empty();
+
+  shuffleArray(towns);
+  $("#towns").append(towns);
+  $("#result").text("Towns shuffled.");
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var oldElement = array[i];
+      array[i] = array[j];
+      array[j] = oldElement;
+    }
+  }
 }
 // function deleteTown() {
 //   let townName = $("#townName").val();
