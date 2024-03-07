d8b04f8fa0918508cce9d7f6b12e9b850e867484
napetkov <n.a.petkov@gmail.com>
2024-03-07 14:59:10 +0200

    Shuffler button + functionality

--- towns.js
+++ towns.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
+	$('#btnShuffle').click(shuffleTowns);
 });
 
 function deleteTown() {
@@ -17,3 +18,21 @@ function deleteTown() {
 	else
 		$('#result').text(townName + " not found.");
 }
+
+function shuffleTowns() {
+	let towns = $('#towns option').toArray();
+	$('#towns').empty();
+	shuffleArray(towns);
+	$('#towns').append(towns);
+	$('#result').text("Towns shuffled.");
+
+	function shuffleArray(array) {
+		for (var i = array.length - 1; i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var oldElement = array[i];
+			array[i] = array[j];
+			array[j] = oldElement;
+		}
+	}
+}
+
